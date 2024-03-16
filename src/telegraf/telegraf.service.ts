ecc4822d02673880e4352368e5ccfc449cc67176
Maksim Shuklin <makshuk@bk.ru>
2024-03-16 11:04:26 +0300

    Добавлены изменения в класс TelegrafService для проверки доступа пользователя

--- src/telegraf/telegraf.service.ts
+++ src/telegraf/telegraf.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { DataManagementService } from 'src/data-management/data-management.service';
 import { LoggerService } from 'src/services/logger/logger.service';
-import { Telegraf } from 'telegraf';
+import { Context, Telegraf } from 'telegraf';
 
 @Injectable()
 export class TelegrafService {
@@ -26,6 +26,20 @@ export class TelegrafService {
       this.logger.warn('Бот уже запущен');
       return `Бот уже запущен: ${this.botRun}`;
     }
+
+    this.bot.use((ctx: Context, next: () => Promise<void>) => {
+      const userId = ctx.from?.id;
+      console.log(userId);
+      if (userId !== 123456) {
+        // Если ID пользователя не проходит проверку, вы можете прервать выполнение
+        // или отправить пользователю сообщение об ошибке
+        ctx.reply('Access denied.');
+        return;
+      }
+
+      // Если ID пользователя проходит проверку, продолжаем выполнение следующего middleware
+      return next();
+    });
 
     this.bot.command('t', (ctx) => {
       console.log(ctx.message.from);
