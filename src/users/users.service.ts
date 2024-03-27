5bde511cd32c04651c70edf65c50e65f12a20ee0
Maksim Shuklin <makshuk@bk.ru>
2024-03-27 17:24:28 +0300

    Изменения в классе UsersService для обработки запросов пользователей

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,155 +6,82 @@ import { PrismaService } from 'src/services/prisma/prisma.service';
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async create(createUserDto: CreateUserDto) {
-    try {
-      const { name, telegramId, roleId } = createUserDto;
-      const role = await this.prisma.role.findUnique({
-        where: { id: roleId },
-        include: { users: true },
-      });
-
-      if (!role) {
-        throw new Error('Роль не найдена');
-      }
-
-      if (role.users.length >= role.maxUsers) {
-        throw new Error(
-          'Превышено максимальное количество пользователей для этой роли',
-        );
-      }
-
-      const newUser = await this.prisma.user.create({
-        data: {
-          name: name,
-          telegramId: telegramId,
-          role: {
-            connect: { id: roleId },
-          },
+  create(createUserDto: CreateUserDto) {
+    const { name, telegramId, roleId } = createUserDto;
+    const request = this.prisma.user.create({
+      data: {
+        name: name,
+        telegramId: telegramId,
+        role: {
+          connect: { id: roleId },
         },
-      });
-      return { error: false, data: newUser, message: 'User created' };
+      },
+    });
+    return this.handleRequest(request, 'User created');
+  }
+
+  findAll() {
+    const request = this.prisma.user.findMany();
+    return this.handleRequest(request, 'Users found');
+  }
+
+  findOne(id: number) {
+    const request = this.prisma.user.findUnique({
+      where: {
+        telegramId: id,
+      },
+    });
+    return this.handleRequest(request, 'User found');
+  }
+
+  update(id: number, updateUserDto: CreateUserDto) {
+    const { name } = updateUserDto;
+    const request = this.prisma.user.update({
+      where: { telegramId: id },
+      data: {
+        name: name,
+      },
+    });
+    return this.handleRequest(request, 'User updated');
+  }
+
+  remove(id: number) {
+    const request = this.prisma.user.delete({
+      where: { telegramId: id },
+    });
+    return this.handleRequest(request, 'User deleted');
+  }
+
+  createNewRole(name: string, maxUsers: number) {
+    const request = this.prisma.role.create({
+      data: {
+        name,
+        maxUsers,
+      },
+    });
+    return this.handleRequest(request, 'Role created successfully');
+  }
+
+  getAllRoles() {
+    const request = this.prisma.role.findMany();
+    return this.handleRequest(request, 'Roles found');
+  }
+
+  userExists(telegramId: number) {
+    const request = this.prisma.user.findUnique({
+      where: { telegramId },
+    });
+    return this.handleRequest(request, 'User found');
+  }
+
+  private async handleRequest<T>(request: Promise<T>, successMessage: string) {
+    try {
+      const data = await request;
+      return { error: false, data, message: successMessage };
     } catch (error) {
       return {
         error: true,
-        message: `createUser error: ${(error as Error).message}`,
-      };
-    }
-  }
-
-  async findAll() {
-    try {
-      const allUsers = await this.prisma.user.findMany();
-      return { error: false, data: allUsers, message: 'Users found' };
-    } catch (error) {
-      return {
-        error: true,
-        message: `getUsers error: ${(error as Error).message}`,
-      };
-    }
-  }
-
-  async findOne(id: number) {
-    try {
-      const user = await this.prisma.user.findUnique({
-        where: {
-          telegramId: id,
-        },
-      });
-      if (!user) {
-        throw new Error('User not found');
-      }
-
-      return { error: false, data: user, message: 'User found' };
-    } catch (error) {
-      return { error: true, message: `getUser error: ${error}` };
-    }
-  }
-
-  async update(id: number, updateUserDto: CreateUserDto) {
-    try {
-      const { name } = updateUserDto;
-      const updatedUser = await this.prisma.user.update({
-        where: { telegramId: id },
-        data: {
-          name: name,
-        },
-      });
-      return { error: false, data: updatedUser, message: 'User updated' };
-    } catch (error) {
-      return {
-        error: true,
-        message: `updateUser error: ${(error as Error).message}`,
-      };
-    }
-  }
-
-  async remove(id: number) {
-    try {
-      const user = await this.prisma.user.findUnique({
-        where: { telegramId: id },
-      });
-      if (!user) {
-        throw new Error('User not found');
-      }
-      if (user.roleId === 1) {
-        return { error: true, message: 'Cannot delete user with role id 1' };
-      }
-      await this.prisma.user.delete({
-        where: { telegramId: id },
-      });
-      return { error: false, message: 'User deleted' };
-    } catch (error) {
-      return {
-        error: true,
-        message: `deleteUser error: ${(error as Error).message}`,
-      };
-    }
-  }
-
-  async createNewRole(name: string, maxUsers: number) {
-    try {
-      const newRole = await this.prisma.role.create({
-        data: {
-          name,
-          maxUsers,
-        },
-      });
-      return {
-        error: false,
-        data: newRole,
-        message: 'Role created successfully',
-      };
-    } catch (error) {
-      console.log(error);
-      return { error: true, message: `createNewRole error: ${error}` };
-    }
-  }
-
-  async getAllRoles() {
-    try {
-      const roles = await this.prisma.role.findMany();
-      return { error: false, data: roles, message: 'Roles found' };
-    } catch (error) {
-      return {
-        error: true,
-        message: `getAllRoles error: ${(error as Error).message}`,
-      };
-    }
-  }
-
-  async userExists(telegramId: number) {
-    try {
-      const user = await this.prisma.user.findUnique({
-        where: { telegramId },
-      });
-      console.log(user);
-      const userExists = user ? true : false;
-      return { error: false, isUserExists: userExists, message: 'User found' };
-    } catch (error) {
-      return {
-        error: true,
-        message: `getUser error: ${(error as Error).message}`,
+        message: `${successMessage} error: ${(error as Error).message}`,
       };
     }
   }
